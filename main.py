14c21b695bfedbb0b3fd6a1ffc9ba0b4392597b4
Anjitha Wijesinghe <140952191+AnjithaSL@users.noreply.github.com>
2023-11-26 17:38:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import numpy as np
 
+#number of dart throw
 darts = 1000
 i = 0
+#number of hits to the circle
 win = 0
 
 for i in range(darts):
 
-    x = np.random.random()
-    y = np.random.random()
-    z = (x**2 + y**2)**0.5
+    x = np.random.random() #-1 to 1
+    y = np.random.random() #-1 to 1
+    z = (x**2 + y**2)**0.5 # search z using Pythagorean theorem
 
-    if z <= 1:
+    if z <= 1:  #  z greater than 1 it hit to circle or not 
         win +=1
 
-value = 4 * win / darts
+value = 4 * win / darts  # calculation of pi
 
 print('PI ={}'.format(value))
